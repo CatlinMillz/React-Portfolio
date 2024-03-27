46c7b6923c02565d0cde0cacb0170187f7bf0554
Catlin Millz <cat@Catlins-MacBook-Pro.local>
2024-03-27 13:01:10 -0400

    changed header name installed dependencies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header(props) {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Cat's Meow Coding</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
